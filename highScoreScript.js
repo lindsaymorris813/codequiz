052549d9560ded9d514c67d09d6f737eb51ca61b
Lindsay Morris <lindsaymorris813@gmail.com>
2020-11-06 22:10:50 -0500

    updating changes to Javascript, adding some functionality (local storage), and fixing bugs

--- highScoreScript.js
+++ highScoreScript.js
@@ -8,23 +8,31 @@ var goBackBtn = document.querySelector("#go-back");
 var highScoreList = document.querySelector("#high-score-list");
 
 //pull high scores from local storage
-var highScoresArray = JSON.parse(localStorage.getItem('scores'));
+var highScoresArray = JSON.parse(localStorage.getItem("scores"));
 
 //add high scores to page
+function showTopScores() {
 for (var i = 0; i < 5; i++) {
+    if (highScoresArray.length !== null) {
     var highScore = document.createElement("li");
-    highScore.textContent= highScoreList[i];
+    highScore.textContent= "Initials: " + highScoresArray[i].initials + " --- Score: " + highScoresArray[i].score;
     highScore.setAttribute("class", "list-group-item");
     highScoreList.appendChild(highScore);
-
+} else {
+    return;
+}
 };
+}
+
 //clear scores button
 clearScoresBtn.addEventListener('click', function() {
     localStorage.clear();
+    var highScoresArray = JSON.parse(localStorage.getItem("scores"));
 });
 
 //go back button restarts game
 goBackBtn.addEventListener('click', function() {
     window.location = "index.html";
 });
-    
+
+showTopScores();
--- script.js
+++ script.js
@@ -11,66 +11,87 @@ var question = document.querySelector("#question");
 var userInitials = document.querySelector("#initials");
 var userSubmitBtn = document.querySelector("#userSubmit");
 var timer = document.querySelector("#timer");
-
+var choiceList = document.querySelector("#choice-list");
+var userAnswer;
+var correctAnswer;
+var timePenalty = 10;
+var q = 0;
+var highScore = [];
 
 //quiz questions array with answers and correct answer index
+function getStoredScores() {
+    var storedScores = JSON.parse(localStorage.getItem("scores"));
+    if (storedScores !== null) {
+        highScore = storedScores;
+    }
+}
 var quizQuestions = [
     {
         question: "Commonly used data types DO NOT include ____",
         choices: ["stings", "numbers", "alerts", "booleans"],
-        answerIndex: 3,
+        answerIndex: "alerts",
     },
     {
         question: "The condition in an if else statement is enclosed within _____",
         choices: ["parentheses", "quotes", "curly brackets", "square brackets"],
-        answerIndex: 3,
+        answerIndex: "curly brackets",
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-        answerIndex: 4,
+        answerIndex: "console.log",
     },
     {
         question: "String values must be enclosed withing ______ when being assigned variables.",
         choices: ["commas", "curly brackets", "quotes", "parentheses"],
-        answerIndex: 3,
+        answerIndex: "quotes",
     },
     {
         question: "Inside which HTML element do we put the JavaScript?",
         choices: ["script", "scripting", "js", "javascript"],
-        answerIndex: 1,
+        answerIndex: "script",
     },
     {
         question: "How do you create a function in JavaScript?",
         choices: ["function: myFunction()", "function myFunction()", "function= myFunction()", "myFunction = function()"],
-        answerIndex: 2,
+        answerIndex: "function myFunction()",
     }
 ];
 
 var secondsLeft = quizQuestions.length * 10;
 
-//hides start div, shows questions and answer choices div
-startBtn.addEventListener('click', function () {
-    startContainer.classList.add("d-none");
-    quizContainer.classList.remove("d-none");
-    runQuestions();
-    score();
-});
+function gameOver() {
+    quizContainer.classList.add("d-none");
+    gameOverContainer.classList.remove("d-none");
+
+}
 //running through questions, loops as you click answers, when all questinos complete or time is out, 
 //hides question container and shows game over container
-function runQuestions() {
-    quizContainer.addEventListener('click', function (event) {
-        for (var i = 1; i < quizQuestions.length; i++);
-        if (event.target.parentElement.id.innerHTML === quizQuestions[i].answerIndex) {
-            
-            question.textContent = quizQuestions[i].question;
-            choiceOne.textContent = quizQuestions[i].choices[0];
-            choiceTwo.textContent = quizQuestions[i].choices[1];
-            choiceThree.textContent = quizQuestions[i].choices[2];
-            choiceFour.textContent = quizQuestions[i].choices[3];
-        }
-    })
+function showQuestions() {
+    if (q >= quizQuestions.length) {
+        gameOver();
+    } else {
+    var correctAnswer = quizQuestions[q].answerIndex;
+    question.textContent = quizQuestions[q].question;
+    choiceOne.textContent = quizQuestions[q].choices[0];
+    choiceTwo.textContent = quizQuestions[q].choices[1];
+    choiceThree.textContent = quizQuestions[q].choices[2];
+    choiceFour.textContent = quizQuestions[q].choices[3];
+    return correctAnswer;
 }
+// to move on to next question
+function nextquestion() {
+    q++;
+    var correctAnswer = quizQuestions[q].answerIndex;
+    question.textContent = quizQuestions[q].question;
+    choiceOne.textContent = quizQuestions[q].choices[0];
+    choiceTwo.textContent = quizQuestions[q].choices[1];
+    choiceThree.textContent = quizQuestions[q].choices[2];
+    choiceFour.textContent = quizQuestions[q].choices[3];
+    return correctAnswer;
+    }
+}
+
 //score/timer count down
 function score() {
     var timerInterval = setInterval(function () {
@@ -78,25 +99,66 @@ function score() {
         timer.textContent = "Score: " + secondsLeft;
 
         if (secondsLeft === 0) {
-            quizContainer.classList.add("d-none");
-            gameOverContainer.classList.remove("d-none");
+            gameOver();
         }
 
     }, 1000);
-}
+};
+//check user selection against correct answer
+choiceList.addEventListener('click', function (event) {
+    event.preventDefault();
+    var userAnswer = (event.target);
 
+    if (userAnswer.innerHTML === correctAnswer) {
+        setTimeout(correctAnswerResponse, 2000);
+        q++
+        showQuestions();
+    } else {
+        setTimeout(wrongAnswerResponse, 2000);
+        q++
+        showQuestions();
+
+    }
+    return userAnswer;
+    gameOver();
+}),
 
 //user submits initials and saves high score & initials to local storage
 userSubmitBtn.addEventListener('click', function (event) {
     event.preventDefault();
-    if (userInitials.innerHTML !== null) {
-        var highScore = {
-            intials: userInitials,
+    getStoredScores();
+    if (userInitials.value !== " ") {
+        var thisGame = {
+            initials: userInitials.value.trim(),
             score: secondsLeft
-        };
-
+            
+        }
+        highScore.push(thisGame);
         localStorage.setItem('scores', JSON.stringify(highScore));
         window.location = "highscore.html";
+    } else {
+        alert("Please enter initials");
     }
+});
 
+function correctAnswerResponse(event) {
+    function playSound() {
+        var a = new Audio(Assets/week7-brrring.mp3);
+        a.play();
+    }
+}
+
+function wrongAnswerResponse(event) {
+    function playSound() {
+        var a = new Audio(Assets/week7-bounce.mp3);
+        a.play();
+    }
+}
+
+//hides start div, shows questions and answer choices div
+startBtn.addEventListener('click', function () {
+    startContainer.classList.add("d-none");
+    quizContainer.classList.remove("d-none");
+    showQuestions();
+    score();
 });
